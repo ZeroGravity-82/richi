ba708f66a036b38a34743b862fff3a5e4dc0fb84
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2020-01-07 15:46:27 +0700

    fix type-hints for operation entity

--- src/Entity/Operation.php
+++ src/Entity/Operation.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\OperationRepository")
@@ -10,6 +11,8 @@ use Doctrine\ORM\Mapping as ORM;
 class Operation
 {
     /**
+     * @var integer
+     *
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
@@ -17,36 +20,53 @@ class Operation
     private $id;
 
     /**
+     * @var UserInterface
+     *
      * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="operations")
      * @ORM\JoinColumn(nullable=false)
      */
     private $user;
 
     /**
+     * @var Account
+     *
      * @ORM\ManyToOne(targetEntity="App\Entity\Account", inversedBy="operations")
      */
     private $source;
 
     /**
+     * @var Account
+     *
      * @ORM\ManyToOne(targetEntity="App\Entity\Account", inversedBy="operations")
      */
     private $target;
 
     /**
+     * @var integer
+     *
      * @ORM\Column(type="integer")
      */
     private $amount;
 
     /**
+     * @var \DateTimeInterface
+     *
      * @ORM\Column(type="datetime")
      */
     private $createdAt;
 
     /**
+     * @var \DateTimeInterface
+     *
      * @ORM\Column(type="datetime")
      */
     private $updatedAt;
 
+    /**
+     * Operation constructor.
+     *
+     * @throws \Exception
+     */
     public function __construct()
     {
         $now             = new \DateTime();
@@ -54,28 +74,47 @@ class Operation
         $this->updatedAt = $now;
     }
 
+    /**
+     * @return integer|null
+     */
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getUser(): ?User
+    /**
+     * @return UserInterface|null
+     */
+    public function getUser(): ?UserInterface
     {
         return $this->user;
     }
 
-    public function setUser(?User $user): self
+    /**
+     * @param UserInterface|null $user
+     *
+     * @return Operation
+     */
+    public function setUser(?UserInterface $user): self
     {
         $this->user = $user;
 
         return $this;
     }
 
+    /**
+     * @return Account|null
+     */
     public function getSource(): ?Account
     {
         return $this->source;
     }
 
+    /**
+     * @param Account|null $source
+     *
+     * @return Operation
+     */
     public function setSource(?Account $source): self
     {
         $this->source = $source;
@@ -83,11 +122,19 @@ class Operation
         return $this;
     }
 
+    /**
+     * @return Account|null
+     */
     public function getTarget(): ?Account
     {
         return $this->target;
     }
 
+    /**
+     * @param Account|null $target
+     *
+     * @return Operation
+     */
     public function setTarget(?Account $target): self
     {
         $this->target = $target;
@@ -95,11 +142,19 @@ class Operation
         return $this;
     }
 
+    /**
+     * @return integer|null
+     */
     public function getAmount(): ?int
     {
         return $this->amount;
     }
 
+    /**
+     * @param integer $amount
+     *
+     * @return Operation
+     */
     public function setAmount(int $amount): self
     {
         $this->amount = $amount;
@@ -107,16 +162,27 @@ class Operation
         return $this;
     }
 
+    /**
+     * @return \DateTimeInterface|null
+     */
     public function getCreatedAt(): ?\DateTimeInterface
     {
         return $this->createdAt;
     }
 
+    /**
+     * @return \DateTimeInterface|null
+     */
     public function getUpdatedAt(): ?\DateTimeInterface
     {
         return $this->updatedAt;
     }
 
+    /**
+     * @param \DateTimeInterface $updatedAt
+     *
+     * @return Operation
+     */
     public function setUpdatedAt(\DateTimeInterface $updatedAt): self
     {
         $this->updatedAt = $updatedAt;
