16f688361fd70261468175d5b908a3a2e9000df2
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2020-01-07 15:43:53 +0700

    fix type-hints for account entity

--- src/Entity/Account.php
+++ src/Entity/Account.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\AccountRepository")
@@ -12,6 +13,8 @@ use Doctrine\ORM\Mapping as ORM;
 class Account
 {
     /**
+     * @var integer
+     *
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
@@ -19,36 +22,53 @@ class Account
     private $id;
 
     /**
+     * @var UserInterface
+     *
      * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="accounts")
      * @ORM\JoinColumn(nullable=false)
      */
     private $user;
 
     /**
+     * @var string
+     *
      * @ORM\Column(type="string", length=255)
      */
     private $name;
 
     /**
+     * @var string
+     *
      * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $icon;
 
     /**
+     * @var ArrayCollection
+     *
      * @ORM\OneToMany(targetEntity="App\Entity\Operation", mappedBy="source")
      */
     private $operations;
 
     /**
+     * @var \DateTimeInterface
+     *
      * @ORM\Column(type="datetime")
      */
     private $createdAt;
 
     /**
+     * @var \DateTimeInterface
+     *
      * @ORM\Column(type="datetime")
      */
     private $updatedAt;
 
+    /**
+     * Account constructor.
+     *
+     * @throws \Exception
+     */
     public function __construct()
     {
         $this->operations = new ArrayCollection();
@@ -58,28 +78,47 @@ class Account
         $this->updatedAt  = $now;
     }
 
+    /**
+     * @return integer|null
+     */
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getUser(): ?User
+    /**
+     * @return UserInterface|null
+     */
+    public function getUser(): ?UserInterface
     {
         return $this->user;
     }
 
-    public function setUser(?User $user): self
+    /**
+     * @param UserInterface|null $user
+     *
+     * @return Account
+     */
+    public function setUser(?UserInterface $user): self
     {
         $this->user = $user;
 
         return $this;
     }
 
+    /**
+     * @return string|null
+     */
     public function getName(): ?string
     {
         return $this->name;
     }
 
+    /**
+     * @param string $name
+     *
+     * @return Account
+     */
     public function setName(string $name): self
     {
         $this->name = $name;
@@ -87,11 +126,19 @@ class Account
         return $this;
     }
 
+    /**
+     * @return string|null
+     */
     public function getIcon(): ?string
     {
         return $this->icon;
     }
 
+    /**
+     * @param string|null $icon
+     *
+     * @return Account
+     */
     public function setIcon(?string $icon): self
     {
         $this->icon = $icon;
@@ -107,6 +154,11 @@ class Account
         return $this->operations;
     }
 
+    /**
+     * @param Operation $operation
+     *
+     * @return Account
+     */
     public function addOperation(Operation $operation): self
     {
         if (!$this->operations->contains($operation)) {
@@ -117,6 +169,11 @@ class Account
         return $this;
     }
 
+    /**
+     * @param Operation $operation
+     *
+     * @return Account
+     */
     public function removeOperation(Operation $operation): self
     {
         if ($this->operations->contains($operation)) {
@@ -130,16 +187,27 @@ class Account
         return $this;
     }
 
+    /**
+     * @return \DateTimeInterface|null
+     */
     public function getCreatedAt(): ?\DateTimeInterface
     {
         return $this->createdAt;
     }
 
+    /**
+     * @return \DateTimeInterface|null
+     */
     public function getUpdatedAt(): ?\DateTimeInterface
     {
         return $this->updatedAt;
     }
 
+    /**
+     * @param \DateTimeInterface $updatedAt
+     *
+     * @return Account
+     */
     public function setUpdatedAt(\DateTimeInterface $updatedAt): self
     {
         $this->updatedAt = $updatedAt;
