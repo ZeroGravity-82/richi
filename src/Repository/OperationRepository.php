b6ed4f7421b3cc018f203e965e3a84e6020e4c3b
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2020-01-08 00:41:04 +0700

    add new income operation handling

--- src/Repository/OperationRepository.php
+++ src/Repository/OperationRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository;
 use App\Entity\Operation;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Common\Persistence\ManagerRegistry;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @method Operation|null find($id, $lockMode = null, $lockVersion = null)
@@ -17,6 +18,22 @@ class OperationRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Operation::class);
+    }
+
+    /**
+     * Returns an operation list for the user.
+     *
+     * @param UserInterface $user
+     *
+     * @return Operation[]
+     */
+    public function findByUser(UserInterface $user)
+    {
+        return $this->createQueryBuilder('o')
+            ->andWhere('o.user = :user')
+            ->setParameter('user', $user)
+            ->getQuery()
+            ->getResult();
     }
 
     // /**
