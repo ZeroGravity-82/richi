3eda5bc7b2126f1f00834027856aca7d46c91917
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2020-01-15 17:26:09 +0700

    fix redirect path after successful authentication

--- src/Security/FormLoginAuthenticator.php
+++ src/Security/FormLoginAuthenticator.php
@@ -94,7 +94,7 @@ class FormLoginAuthenticator extends AbstractFormLoginAuthenticator implements P
             return new RedirectResponse($targetPath);
         }
 
-        return new RedirectResponse($this->urlGenerator->generate('home'));
+        return new RedirectResponse($this->urlGenerator->generate('operation_index'));
     }
 
     protected function getLoginUrl()
