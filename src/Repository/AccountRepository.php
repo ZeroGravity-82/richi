3aefd803db7f0b78a22184a8021979ba18a214e1
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2020-01-31 15:54:21 +0700

    cut commented out code from repos

--- src/Repository/AccountRepository.php
+++ src/Repository/AccountRepository.php
@@ -72,33 +72,4 @@ class AccountRepository extends ServiceEntityRepository
     {
         return sort($result, SORT_STRING);
     }
-
-    // /**
-    //  * @return Account[] Returns an array of Account objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('a')
-            ->andWhere('a.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('a.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Account
-    {
-        return $this->createQueryBuilder('a')
-            ->andWhere('a.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
