7ce49a304dd1d21e4d1b6bc6a3705c7f38f0b751
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2020-02-18 12:31:57 +0700

    add more info about operation to delete confirmation prompt

--- assets/js/operation.js
+++ assets/js/operation.js
@@ -5,10 +5,11 @@ document.addEventListener('DOMContentLoaded', function () {
     document.addEventListener('click', function (event) {
         const target = event.target;
         if (target.matches('.js-operation-delete')) {
-            const deleteUrl   = target.dataset.url;
-            // const operationName = target.dataset.name;
+            const deleteUrl            = target.dataset.url;
+            const operationDescription = target.dataset.description;
+            const operationAmount      = target.dataset.amount;
 
-            if (confirm(`Do you really want to delete the operation?`)) {
+            if (confirm(`Do you really want to delete operation "${operationDescription}" (${operationAmount})?`)) {
                 deleteOperation(deleteUrl)
                     .then(() => location.reload())
                     .catch(error => console.error(error));
